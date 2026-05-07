9776cb7f655caad98ddba1fada864983aec134d8
Rahul-Elma-Dev <rahul@elma.ltd>
2026-05-07 18:41:30 +0530

    changelog : code refactor

--- src/controllers/attendance.js
+++ src/controllers/attendance.js
@@ -1,14 +1,26 @@
 const Attendance = require("../models/attendance");
+const user = require("../models/users");
 
 exports.markAttendance = async (req, res) => {
-  if (req.user.role !== "student") {
-    return res.status(403).json({ message: "Forbidden" });
-  }
+ 
+
+
+  const{student_id,
+        session_id,
+        status}=req.body;
+
+          const user=await user.findById(student_id);
+
+          if(user?.role!=='student'){
+            return res.status(403).json({message:'Forbidden'});
+
+          }
+
 
   const attendance = await Attendance.create({
-    session_id: req.body.session_id,
-    student_id: req.user._id,
-    status: req.body.status,
+    session_id: session_id,
+    student_id: student_id,
+    status: status,
   });
 
   res.json(attendance);
